0d33dcb5a8c4b89d210b65f1ffbc2f95d0757a5e
Davide Sarra <davide.sarra@gmail.com>
2020-12-17 16:17:13 +0000

    Add Identicon.build_grid/1

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -12,5 +13,20 @@ defmodule Identicon do
 
   def pick_color(%Image{hex: [r, g, b | _tail]} = image) do
       %Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second, _third] = row
+    row ++ [second, first]
   end
 end
