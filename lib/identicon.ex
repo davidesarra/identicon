997cf805f0f2ec8a0fc61a476758baf71b35d909
Davide Sarra <davide.sarra@gmail.com>
2020-12-18 17:51:23 +0000

    Add Identicon.safe_image/2

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
+    |> safe_image(input)
   end
 
   def hash_input(input) do
@@ -61,5 +62,9 @@ defmodule Identicon do
       :egd.filledRectangle(image, start, stop, fill)
     end
     :egd.render(image)
+  end
+
+  def safe_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
